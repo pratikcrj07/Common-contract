e3373e89da90d86d5fca8328340579b20f0367dc
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-16 23:43:05 +0545

    fin(DriverOfferDto)com with serializable

--- src/main/java/com/RideSharing/Common/Dto/DriverOfferDTO.java
+++ src/main/java/com/RideSharing/Common/Dto/DriverOfferDTO.java
@@ -5,6 +5,59 @@ import java.io.Serializable;
 public class DriverOfferDTO  implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private Long rideId;
+    private Long driverId;
+    private double pickupLat;
+    private double pickupLng;
+    private double Fare;
+    private int timeoutSeconds;
+
+    public DriverOfferDTO() {
+
+    }
+    public DriverOfferDTO(
+            Long rideId,
+            Long driverId,
+            double pickupLat,
+            double pickupLon,
+            double estimatedFare,
+            int timeoutSeconds
+    ) {
+        this.rideId = rideId;
+        this.driverId = driverId;
+        this.pickupLat = pickupLat;
+        this.pickupLon = pickupLon;
+        this.estimatedFare = estimatedFare;
+        this.timeoutSeconds = timeoutSeconds;
+    }
+
+    public Long getRideId() {
+        return rideId;
+    }
+
+    public Long getDriverId() {
+        return driverId;
+    }
+
+    public double getPickupLat() {
+        return pickupLat;
+    }
+
+    public double getPickupLon() {
+        return pickupLon;
+    }
+
+    public double getEstimatedFare() {
+        return estimatedFare;
+    }
+
+    public int getTimeoutSeconds() {
+        return timeoutSeconds;
+    }
+}
+
+
+
 
 
 
