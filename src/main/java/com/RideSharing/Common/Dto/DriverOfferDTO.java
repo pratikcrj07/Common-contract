722b770f5c1dec197db9349e4d0f3fbc530a7308
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-19 23:36:57 +0545

    final build ready for maven jar

--- src/main/java/com/RideSharing/Common/Dto/DriverOfferDTO.java
+++ src/main/java/com/RideSharing/Common/Dto/DriverOfferDTO.java
@@ -1,8 +1,10 @@
 package com.RideSharing.Common.Dto;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class DriverOfferDTO  implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Long rideId;
