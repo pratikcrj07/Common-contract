b90e94cc86c92af7570166ddd6b4edda535cc7ee
pratikcrj07 <pratikhumagaincr7@gmail.com>
2026-01-21 15:10:23 +0545

    impl getters and encapsulated through private for lat lng

--- src/main/java/com/RideSharing/Common/Event/RideEvent.java
+++ src/main/java/com/RideSharing/Common/Event/RideEvent.java
@@ -9,42 +9,37 @@ public class RideEvent implements Serializable {
     private Long rideId;
     private Long userId;
     private Long driverId;
+
+    private Double pickupLat;
+    private Double pickupLon;
+
     private Instant timestamp;
 
     public RideEvent() {}
-
 
     public RideEvent(
             RideEventType type,
             Long rideId,
             Long userId,
             Long driverId,
+            Double pickupLat,
+            Double pickupLon,
             Instant timestamp
     ) {
         this.type = type;
         this.rideId = rideId;
         this.userId = userId;
         this.driverId = driverId;
+        this.pickupLat = pickupLat;
+        this.pickupLon = pickupLon;
         this.timestamp = timestamp;
     }
 
-    public RideEventType getType() {
-        return type;
-    }
-
-    public Long getRideId() {
-        return rideId;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public Long getDriverId() {
-        return driverId;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
-    }
+    public RideEventType getType() { return type; }
+    public Long getRideId() { return rideId; }
+    public Long getUserId() { return userId; }
+    public Long getDriverId() { return driverId; }
+    public Double getPickupLat() { return pickupLat; }
+    public Double getPickupLon() { return pickupLon; }
+    public Instant getTimestamp() { return timestamp; }
 }
